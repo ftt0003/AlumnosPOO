bd6bc4d48e6515fedaf24de0f0c0529c10652c7b
al428546 <al428546@uji.es>
2024-11-18 18:34:14 +0100

    Nuevo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Asignatura asig = new Asignatura("Progamacion");
         Alumno alu = new Alumno("68639664M","Lucas","Fernandez", asig);
+        Alumno alu2 = new Alumno("40642391B","Daniel","Santome", asig);
         System.out.println("Asignatura: " + asig);
         System.out.println(alu);
+        System.out.println(alu2);
     }
 }
